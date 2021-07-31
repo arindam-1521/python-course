ec35fd06cee8fb5e445770f04a414accfdc9ab2c
Joy-3 <pradhanarindam3214@gmail.com>
2021-07-31 23:27:03 +0530

    new

--- python practise problem solving/tute1.py
+++ python practise problem solving/tute1.py
@@ -1,3 +1,4 @@
 dict = {'Joy':'boy','Maitri':'didi','Anwesa':"friend"}
 a = input("please choose a word from the dictionary to know it's meaning very easily:")
 print(dict[a.title()])
+print("This is new word")
