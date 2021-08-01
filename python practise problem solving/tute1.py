1157c6897f722f36fba41f2ab84b55ed65b8ec52
Joy-3 <pradhanarindam3214@gmail.com>
2021-08-01 08:27:13 +0530

    new change

--- python practise problem solving/tute1.py
+++ python practise problem solving/tute1.py
@@ -2,3 +2,4 @@ dict = {'Joy':'boy','Maitri':'didi','Anwesa':"friend"}
 a = input("please choose a word from the dictionary to know it's meaning very easily:")
 print(dict[a.title()])
 print("This is new word")
+print("I know a very little amount of coding as a begginer.")
